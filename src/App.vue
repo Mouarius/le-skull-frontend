27a2a41aaa82459c0608523a51de73c29a152330
mouarius <mouarius@gmail.com>
2021-11-04 20:03:37 +0100

    Basic init config for Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
+  <div class="app">
     <h1>Le Skull</h1>
+    <button @click="handleConnectButton">connect</button>
+  </div>
 </template>
 
 <script>
+import { io } from "socket.io-client";
+
 export default {
-    name: "App",
+  name: "App",
+  methods: {
+    handleConnectButton() {
+      console.log("connect");
+    },
+  },
+  created() {
+    const socket = io("http://localhost:4000");
+    socket.on("connect", (socket) => {
+      console.log("Connected to the server");
+    });
+  },
 };
 </script>
 
