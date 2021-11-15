2f7ca98ef3d39400df8370930bb70c799ee122f8
mouarius <mouarius@gmail.com>
2021-11-16 00:42:48 +0100

    Changed LoginForm to LoginPanel

---
+++ src/components/LoginPanel.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="login-panel center-panel card">
+    <h2>login form</h2>
+    <form class="login-form" @submit="handleLoginFormSubmit">
+      <div class="username-input">
+        <input
+          type="text"
+          name="input-username"
+          id="input-username"
+          v-model="inputUsername"
+          placeholder="username"
+          required
+        />
+      </div>
+      <div class="submit-buttons">
+        <input
+          @click="handleCreateGameButton"
+          type="button"
+          value="create a room"
+        />
+        <p>or</p>
+        <div class="join-room">
+          <input
+            type="text"
+            name="room-id"
+            id="input-room-id"
+            v-model="inputRoomId"
+            placeholder="room code"
+          />
+          <input
+            @click="handleJoinGameButton"
+            type="button"
+            value="join a room"
+          />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { API_URL } from "../config/env";
+
+export default {
+  name: "login-panel",
+  props: ["updatePlayer"],
+  data() {
+    return {
+      inputUsername: "",
+      inputRoomId: "",
+    };
+  },
+  methods: {
+    handleCreateGameButton(e) {
+      e.preventDefault();
+      axios
+        .post(API_URL + "/room/", { username: this.inputUsername })
+        .then((res) => {
+          const { room, user } = res.data;
+          console.log(result.data);
+          this.updatePlayer(user);
+        });
+    },
+    handleJoinGameButton(e) {
+      e.preventDefault();
+      axios
+        .post(API_URL + `/room/${this.inputRoomId}`, {
+          username: this.inputUsername,
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
+    },
+    handleLoginFormSubmit(e) {
+      e.preventDefault();
+    },
+    createRoom() {
+      axios.post(API_URL + "/room", {});
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+label {
+  font-size: 0.8em;
+  font-weight: 200;
+  text-align: left;
+}
+.login-form {
+  display: flex;
+  flex-direction: column;
+  .username-input {
+    display: flex;
+    flex-direction: column;
+    margin-top: 12px;
+  }
+}
+.submit-buttons {
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+  text-align: center;
+  .join-room {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
